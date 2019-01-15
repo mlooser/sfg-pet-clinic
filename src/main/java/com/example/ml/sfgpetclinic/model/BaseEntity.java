842a2bfc46a75b9a7bf1925068849e9e478cd41e
mlooser <mlooser@gmail.com>
2019-01-15 11:01:58 +0100

    Add JPA mappengs to BaseEntity. closes #26.

--- src/main/java/com/example/ml/sfgpetclinic/model/BaseEntity.java
+++ src/main/java/com/example/ml/sfgpetclinic/model/BaseEntity.java
@@ -2,8 +2,16 @@ package com.example.ml.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable, IdEntity<Long>{
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
