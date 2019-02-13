425b07196381ed2048650b9afc62c86c71673de2
mlooser <mlooser@gmail.com>
2019-02-13 15:00:00 +0100

    Configuration to use JUnit 5.

--- src/test/java/com/example/ml/sfgpetclinic/controller/IndexControllerTest.java
+++ src/test/java/com/example/ml/sfgpetclinic/controller/IndexControllerTest.java
@@ -3,11 +3,7 @@ package com.example.ml.sfgpetclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class IndexController {
+public class IndexControllerTest {
 
-	@RequestMapping({"","/","index","index.html"})
-	public String index() {
-		return "index";
-	}
+
 }
